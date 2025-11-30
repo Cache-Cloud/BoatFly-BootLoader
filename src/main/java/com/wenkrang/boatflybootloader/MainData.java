6c8af556868a70aa8ee9abd7db51be77bbe1e78e
乔栀 <36266781+Cache-Cloud@users.noreply.github.com>
2025-11-30 18:41:05 +0800

    Update plugin version and number in MainData

--- src/main/java/com/wenkrang/boatflybootloader/MainData.java
+++ src/main/java/com/wenkrang/boatflybootloader/MainData.java
@@ -14,12 +14,12 @@ public class MainData {
     public static boolean IsShutDown = false;
 
     //该变量用于储存插件的名字
-    public static String PluginName = "1.5.8";
+    public static String PluginName = "1.5.10";
     //该变量用于储存该插件对象
     public static Plugin plugin = null;
     //该变量用于储存该插件文件对象
     public static File PluginFile = null;
     //该变量用于储存版本号
-    public static int Number = 12;
+    public static int Number = 15;
 
 }
